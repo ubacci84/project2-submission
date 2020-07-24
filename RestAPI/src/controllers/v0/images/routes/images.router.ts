b3c46fc9a7a7e95b1efbaf00d603f55debd7e58c
Remy F <remyf@insightbb.com>
2020-07-23 21:09:28 -0400

    fixed synced res. with tmp file delete

--- RestAPI/src/controllers/v0/images/routes/images.router.ts
+++ RestAPI/src/controllers/v0/images/routes/images.router.ts
@@ -20,6 +20,9 @@ const router: Router = Router();
 /**************************************************************************** */
 router.get( "/filteredimage", async ( req, res ) => {
 
+//////////////////////////////////////
+//    1. validate the image_url query 
+//////////////////////////////////////   
 let { image_url } = req.query;
 // Check first if we have the url
 if ( !image_url ) {
@@ -27,29 +30,79 @@ if ( !image_url ) {
             .send(`Image url required`);  
 }   
 
-// Filter the file
+////////////////////////////////////////////////////////////////
+//    2. call filterImageFromURL(image_url) to filter the image
+////////////////////////////////////////////////////////////////
 let filtered_image_url = await filterImageFromURL(image_url);
 
+// Send the file to the server
+// TODO ************************************
+
+/////////////////////////////////////////////////
+//    3. send the resulting file in the response
+/////////////////////////////////////////////////
 // Failed?
 if(!filtered_image_url){
     return res.status(422)
     .send(`File could not be processed!`);      
 }
 
-// Send the file to the server
-// TODO ************************************
+    console.log('avant1');
+    res.status(200).sendFile(filtered_image_url, (err) => {
+        if (err) {
+            console.log('Error');
+        }
+        else{
+            var files:string[] = new Array();
+            files.push(filtered_image_url.toString());
+            deleteLocalFiles(files);
+            console.log('apres2');
+        }
+    });
+    console.log('apres1');
+  
+/*
+ res.status(200)
+    //      .send(`Image filtering complete`)
+    //      .send(files)
+    //        .send(image_url)
+    //    .send(filtered_image_url)
+        .sendFile(filtered_image_url)
+  }
+*/
 
-// Delete the local file
+//    4. deletes any files on the server on finish of the response
+/*
+console.log('avant2');
 var files:string[] = new Array();
 files.push(filtered_image_url.toString());
 deleteLocalFiles(files);
-
-res.status(200)
-//      .send(`Image filtering complete`)
-//      .send(files)
-//        .send(image_url)
-    .send(filtered_image_url)
+console.log('apres2');
+*/
 } );
+/*
+let respondFirst = function(strParam: string){
+    return new Promise(function(resolve){
+      // Prepare the parameters
+      var bucketParams = {
+        Bucket: c.aws_media_bucket,
+      };
+  
+      // Call S3 to obtain a list of the objects in the bucket 
+      s3.listObjects(bucketParams, (err: Error, data: any) => {
+        if (err) {
+  
+          console.log("Error", err);
+          reject(`Error ${err}`);
+        } else {
+          console.log('Param: ' + strParam);
+  //        console.log("Success", data);
+          resolve(data.Contents[0]);
+        }
+      });
+    });
+  }
+*/  
 
 //! END @TODO1
 
